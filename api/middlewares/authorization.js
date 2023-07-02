fd42468e2ed331f12c47c1e6c0b0ce357c4eceac
haamym <jenrid@live.com>
2023-07-02 17:11:26 +0800

    token verification middleware added

---
+++ api/middlewares/authorization.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+module.exports = async (req,res,next)=>{
+    try {
+
+        const jwtToken = req.header("token");
+
+        if(!jwtToken){
+            res.status(403).json('Not Authorized');
+        }
+        const payload = jwt.verify(jwtToken,process.env.SECRET);
+
+        req.user = payload.user;
+        
+    } catch (error) {
+        console.error(error.message)
+        return res.status(403).json('Not Authorized');
+    }
+}
