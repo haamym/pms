aa37718ae5b99a74b81ad15667eac98cc5a7468c
haamym <jenrid@live.com>
2023-07-24 15:08:25 +0800

    cors deleted

--- api/server.js
+++ api/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const port = process.env.PORT || 2000;
 const userRouter = require('./routes/userRoutes')
 const authRouter = require('./routes/authRoutes')
@@ -7,18 +6,8 @@ const dashboardRoute = require('./routes/dashboardRoute');
 
 const app = express();
 app.use(express.json())
-// app.use(cors)
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200); 
-    } else {
-        next(); 
-    }
-  });
+
 
 app.get('/',(req, res)=>{
    res.json(`ok its working`)
