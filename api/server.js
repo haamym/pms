ba9ea7e1fb4b3def1058b8464d3f5bcbd6feab2d
haamym <jenrid@live.com>
2023-07-24 14:21:28 +0800

    access added for local 3000

--- api/server.js
+++ api/server.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(express.json())
 app.use(cors)
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.get('/',(req, res)=>{
    res.json(`ok its working`)
 })
