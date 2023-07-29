5bc184c124d2cff1874b4b4ac8ff2869ca4c175f
haamym <jenrid@live.com>
2023-07-29 12:50:37 +0800

    custom work for tables

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,88 +1,44 @@
-import {Card,Table,TableHead,TableRow,TableHeaderCell,TableBody,TableCell,Text,Title,Badge,} from "@tremor/react";
-import { StatusOnlineIcon } from "@heroicons/react/outline";
+
+import { StatusOnlineIcon} from "@heroicons/react/outline";
+
+export default function TableComp() {
 
 
-export default function TableComp(){
+  return (
+    <table className="mx-2 text-sm w-full">
+      <thead className="bg-[#3ea7e2] text-left text-[white] font-bold ">
+        <tr className="">
+          <th className="px-4 py-3">Username</th>
+          <th className="px-4 py-3">Online</th>
+          <th className="px-4 py-3">Actions</th>
+          <th className="px-4 py-3"></th>
+        </tr>
+      </thead>
 
-    const data = [
-        {
-          name: "Viola Amherd",
-          Role: "Federal Councillor",
-          departement: "The Federal Department of Defence, Civil Protection and Sport (DDPS)",
-          status: "active",
-        },
-        {
-          name: "Simonetta Sommaruga",
-          Role: "Federal Councillor",
-          departement:
-            "The Federal Department of the Environment, Transport, Energy and Communications (DETEC)",
-          status: "active",
-        },
-        {
-          name: "Alain Berset",
-          Role: "Federal Councillor",
-          departement: "The Federal Department of Home Affairs (FDHA)",
-          status: "active",
-        },
-        {
-          name: "Ignazio Cassis",
-          Role: "Federal Councillor",
-          departement: "The Federal Department of Foreign Affairs (FDFA)",
-          status: "active",
-        },
-        {
-          name: "Ueli Maurer",
-          Role: "Federal Councillor",
-          departement: "The Federal Department of Finance (FDF)",
-          status: "active",
-        },
-        {
-          name: "Guy Parmelin",
-          Role: "Federal Councillor",
-          departement: "The Federal Department of Economic Affairs, Education and Research (EAER)",
-          status: "active",
-        },
-        {
-          name: "Karin Keller-Sutter",
-          Role: "Federal Councillor",
-          departement: "The Federal Department of Justice and Police (FDJP)",
-          status: "active",
-        },
-      ];
+      <tbody className="">
+        <tr className="">
+          <td className="px-4 py-3">Username</td>
+          <td className="px-4 py-3">
+            <StatusOnlineIcon className="h-5 w-5 text-green-500" />
+          </td>
+          <td className="px-4 py-3">
+            action
+          </td>
+          <td className="px-4 py-3">
+            <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+            </svg>
+            </button>
+          </td>
+        </tr>
+        
       
+      </tbody>
 
-
-    return(
-        <Card className="w-3/4">
-    <Title>List of Swiss Federal Councillours</Title>
-    <Table className="mt-5">
-      <TableHead>
-        <TableRow>
-          <TableHeaderCell>Name</TableHeaderCell>
-          <TableHeaderCell>Position</TableHeaderCell>
-          <TableHeaderCell>Department</TableHeaderCell>
-          <TableHeaderCell>Status</TableHeaderCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((item) => (
-          <TableRow key={item.name}>
-            <TableCell>{item.name}</TableCell>
-            <TableCell>
-              <Text>{item.Role}</Text>
-            </TableCell>
-            <TableCell>
-              <Text>{item.departement}</Text>
-            </TableCell>
-            <TableCell>
-              <Badge color="emerald" icon={StatusOnlineIcon}>
-                {item.status}
-              </Badge>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </Card>
-    )
+      <tfoot>
+      
+      </tfoot>
+    </table>
+  );
 }
