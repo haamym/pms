d63d2ee5054daa3e9f4258cc1a81ee73754bc227
haamym <jenrid@live.com>
2023-08-02 22:20:11 +0800

    component rendering

--- client/src/pages/Reports.js
+++ client/src/pages/Reports.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import React from 'react'
+import SideNav from "../components/sideNav";
+import TopNav from "../components/topNav";
+import Facility from "../components/Facilities";
 
 export default function Reports() {
   return (
-    <div>Reports</div>
+    <section className="flex flex-col">
+      <header className="flex items-center justify-between px-2 py-2 shadow">
+        <TopNav />
+      </header>
+      <div className="flex  lg:h-[calc(100vh-80px)]">
+        <SideNav />
+        <div className="w-screen px-4 py-2">
+          <div className='flex '>
+            <button className='bg-transparent focus:bg-primary focus:text-[white] focus:shadow-lg hover:bg-primary text-[blue] font-semibold hover:text-[white] py-2 px-4 border border- hover:border-transparent rounded'>Create</button>
+          </div>
+          <div>
+
+          <Facility/>
+          </div>
+          
+            
+        </div>
+      </div>
+    </section>
   )
 }
