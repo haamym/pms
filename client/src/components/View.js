9e43328ff9d281a273be1ce5ae3726b003bdd244
haamym <jenrid@live.com>
2023-08-08 11:22:27 +0800

    view

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -3,8 +3,8 @@
 export default function View() {
   return (
     //make the section absolute and make the section dark and full screen of the website 
-    <section>
-        <div className="w-2/4">
+    
+        <div className="w-2/4 absolute bg-primary">
             <div className="flex justify-between">
                 <p>Name</p>
                 <p>Location</p>
@@ -15,6 +15,6 @@ export default function View() {
             description description description description description description description description 
             </p>
         </div>
-    </section>
+    
   )
 }
--- client/src/pages/Facilities.js
+++ client/src/pages/Facilities.js
@@ -13,11 +13,11 @@ export default function Facilities() {
       </header>
       <div className="flex  lg:h-[calc(100vh-80px)]">
         <SideNav />
-        <div className="w-screen px-4 py-2">
+        <div className="w-screen px-4 py-2 relative">
           <div className='flex '>
             <button className='bg-transparent focus:bg-primary focus:text-[white] focus:shadow-lg hover:bg-primary text-[blue] font-semibold hover:text-[white] py-2 px-4 border border- hover:border-transparent rounded'>Create</button>
           </div>
-          <div>
+          <div className='w-full h-full'>
 
           {/* <Facility/> */}
           <View/>
